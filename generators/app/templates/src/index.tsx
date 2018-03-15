e210267376139416f530ab6e3a5e7b02be30f18e
Shirasawa <764798966@qq.com>
2018-03-15 23:06:04 +0800

    修复语法错误

--- generators/app/templates/src/index.tsx
+++ generators/app/templates/src/index.tsx
@@ -6,12 +6,15 @@ console.log('Hello World!')
 interface AppProps { text: string }
 class App extends React.Component<AppProps, {}> {
   render () {
-    return <div>
-      <h1>{this.props.text}</h1>
-      <h3>(按下F12发现精彩)</h3>
-      <p>:D</p>
-    </div>
+    return (
+      <div>
+        <h1>{this.props.text}</h1>
+        <h3>(按下F12发现精彩)</h3>
+        <p>:D</p>
+      </div>
+    )
   }
 }
 
-render(<App text='Hello World!' />, document.getElementById('app'))
+const app: any = <App text='Hello World!' />
+render(app, document.getElementById('app'))
